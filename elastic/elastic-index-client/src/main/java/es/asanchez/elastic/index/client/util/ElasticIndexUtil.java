b009abb3fa47903223dc226f389fda690ea0a213
Ale <alexsan1998@gmail.com>
2024-03-28 12:55:07 +0100

    Elastic index-client

---
+++ elastic/elastic-index-client/src/main/java/es/asanchez/elastic/index/client/util/ElasticIndexUtil.java
@@ -0,0 +1,22 @@
+package es.asanchez.elastic.index.client.util;
+
+import es.asanchez.elastic.model.index.api.IndexModel;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ElasticIndexUtil<T extends IndexModel> {
+
+    public List<IndexQuery> getIndexQueries(List<T> documents) {
+        return documents.stream()
+                .map(document -> new IndexQueryBuilder()
+                        .withId(document.getId())
+                        .withObject(document).build()
+                ).toList();
+    }
+
+
+}
