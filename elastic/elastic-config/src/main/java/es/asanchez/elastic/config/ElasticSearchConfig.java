5f566a64dd6d6dca273b701bcfaeb2aa368597eb
Ale <alexsan1998@gmail.com>
2024-03-28 12:30:01 +0100

    Elastic config

---
+++ elastic/elastic-config/src/main/java/es/asanchez/elastic/config/ElasticSearchConfig.java
@@ -0,0 +1,25 @@
+package es.asanchez.elastic.config;
+
+import es.asanchez.app.config.ElasticConfigData;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    @Override
+    @NonNull
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticConfigData.getConnectionUrl())
+                .withConnectTimeout(elasticConfigData.getConnectTimeoutMs())
+                .withSocketTimeout(elasticConfigData.getSocketTimeout())
+                .build();
+    }
+}
