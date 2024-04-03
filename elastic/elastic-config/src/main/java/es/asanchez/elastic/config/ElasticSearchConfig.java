2337e05dd9452426bd640b69abc0bde0d822b30c
Ale <alexsan1998@gmail.com>
2024-04-03 19:22:18 +0200

    Cambios para que funcione elasticsearch

--- elastic/elastic-config/src/main/java/es/asanchez/elastic/config/ElasticSearchConfig.java
+++ elastic/elastic-config/src/main/java/es/asanchez/elastic/config/ElasticSearchConfig.java
@@ -1,27 +1,19 @@
 package es.asanchez.elastic.config;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.ElasticsearchTransportBase;
 import es.asanchez.app.config.ElasticConfigData;
 import lombok.NonNull;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "es.asanchez.elastic.index.client.repository")
+@RequiredArgsConstructor
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     private final ElasticConfigData elasticConfigData;
-
-    public ElasticSearchConfig(ElasticConfigData elasticConfigData) {
-        this.elasticConfigData = elasticConfigData;
-    }
 
     @Override
     @NonNull
@@ -32,5 +24,7 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
                 .withSocketTimeout(elasticConfigData.getSocketTimeoutMs())
                 .build();
     }
+
+
 
 }
--- elastic/elastic-model/src/main/java/es/asanchez/elastic/model/index/TwitterIndexModel.java
+++ elastic/elastic-model/src/main/java/es/asanchez/elastic/model/index/TwitterIndexModel.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import es.asanchez.elastic.model.index.api.IndexModel;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -13,18 +14,17 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
-@Document(indexName = "#{elasticConfigData.indexName}")
+//TODO: Ver si se puede cambiar por el valor dinamico de elasticConfigData.indexName
+@Document(indexName = "twitter-index")
 public class TwitterIndexModel implements IndexModel {
 
-    @JsonProperty
+    @Id
     private String id;
-    @JsonProperty
     private Long userId;
-    @JsonProperty
     private String text;
 
-    @Field(type = FieldType.Date, format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ssZZ")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ssZZ")
+    @Field(type = FieldType.Date, format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "uuuu-MM-dd'T'HH:mm:ss")
     @JsonProperty
     private LocalDateTime createdAt;
 
